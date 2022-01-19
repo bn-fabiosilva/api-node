cb8107bd85f4c894dd3aeb26d5f2526e9903ebe3
Fabio <51767749+bn-fabiosilva@users.noreply.github.com>
2022-01-19 15:05:49 -0300

    Ajuste identacao.

--- config/express.js
+++ config/express.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 const config     = require('config');
 
 module.exports = () => {
-    const app = express();
+  const app = express();
 
-    // VARIAVEIS DA APLICACAO
-    app.set('port', process.env.PORT || config.get('server.port'));
+  // VARIAVEIS DA APLICACAO
+  app.set('port', process.env.PORT || config.get('server.port'));
 
-    // MIDDLEWARES
-    app.use(bodyParser.json());
+  // MIDDLEWARES
+  app.use(bodyParser.json());
 
-    return app;
+  return app;
 };
